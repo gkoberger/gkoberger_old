ad60e170df2e3aa1732c0877d16e0fb657590b63
Gregory Koberger <gkoberger@gmail.com>
2011-06-07 23:05:00 -0700

    Adding 404 stuff!

---
+++ app-base/404.php
@@ -0,0 +1,8 @@
+<?php
+
+// This script is going to check to see this page exists on p.gko.net
+if(file_exists('../../p.gkoberger' . $_SERVER["REDIRECT_SCRIPT_URL"])) {
+	Header( "HTTP/1.1 301 Moved Permanently" );
+	Header( "Location: http://www.p.gkoberger.net" . $_SERVER["REDIRECT_SCRIPT_URL"]); 
+}
+
