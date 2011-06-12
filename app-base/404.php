b21c060fbaa74e1dea93e03cd27169b23f5188e9
Gregory Koberger <gkoberger@gmail.com>
2011-06-11 21:03:58 -0700

    Fixing the 404 url

--- app-base/404.php
+++ app-base/404.php
@@ -1,7 +1,7 @@
 <?php
 
 // This script is going to check to see this page exists on p.gko.net
-if(file_exists('../../p.gkoberger' . $_SERVER["REDIRECT_SCRIPT_URL"])) {
+if(file_exists('../../p.gkoberger.net' . $_SERVER["REDIRECT_SCRIPT_URL"])) {
 	Header( "HTTP/1.1 301 Moved Permanently" );
 	Header( "Location: http://www.p.gkoberger.net" . $_SERVER["REDIRECT_SCRIPT_URL"]); 
 }
