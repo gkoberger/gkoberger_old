93fcec6730361cc33cff2c50d387d132debd1b71
Gregory Koberger <gkoberger@gmail.com>
2012-03-29 14:52:23 -0400

    Fix 404s

--- app-base/404.php
+++ app-base/404.php
@@ -1,9 +1,20 @@
 <?php
 
+function url_exists($url) {
+    $ch = @curl_init($url);
+    @curl_setopt($ch, CURLOPT_HEADER, TRUE);
+    @curl_setopt($ch, CURLOPT_NOBODY, TRUE);
+    @curl_setopt($ch, CURLOPT_FOLLOWLOCATION, FALSE);
+    @curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+    $status = array();
+    preg_match('/HTTP\/.* ([0-9]+) .*/', @curl_exec($ch) , $status);
+    return ($status[1] == 200);
+}
+
 // This script is going to check to see this page exists on p.gko.net
-if(file_exists('../../p.gkoberger.net' . $_SERVER["REDIRECT_SCRIPT_URL"])) {
-	Header( "HTTP/1.1 301 Moved Permanently" );
-	Header( "Location: http://www.p.gkoberger.net" . $_SERVER["REDIRECT_SCRIPT_URL"]);
+if(url_exists('http://p.gkoberger.net' .  $_SERVER["REDIRECT_SCRIPT_URL"]) || url_exists('http://p.gkoberger.net' .  $_SERVER["REDIRECT_SCRIPT_URL"] . '/')) {
+        Header( "HTTP/1.1 301 Moved Permanently" );
+        Header( "Location: http://www.p.gkoberger.net" . $_SERVER["REDIRECT_SCRIPT_URL"]);
 }
 
 $filename = "p404.html";
